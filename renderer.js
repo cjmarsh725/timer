90ffa7a0c669af70e97b73ece5485c31e1854e42
Chris <cjmarsh725@gmail.com>
2019-05-31 20:12:27 -0700

    basic electron window and styling

---
+++ renderer.js
@@ -0,0 +1,8 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const remote = require('electron').remote; 
+
+document.getElementById("minimize-btn").addEventListener("click", function (e) {
+  document.getElementById("minimize-btn").append("foo");
+});
