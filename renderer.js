1ca745fa54135f9a434d8e5040e2d3b68c8b04ce
Chris <cjmarsh725@gmail.com>
2019-07-24 13:07:21 -0700

    Changed from pomodoro to regular timer

--- renderer.js
+++ renderer.js
@@ -32,56 +32,36 @@ document.getElementById("close-btn").addEventListener("click", function (e) {
   window.close();
 });
 
+
+/* Timer */
+
 const formatTime = seconds => {
   return new Date(seconds * 1000).toISOString().substr(14, 5);
 }
 
-
-/* Timer */
-
-document.getElementById("timer").innerHTML = formatTime(0);
+// document.getElementById("timer").innerHTML = formatTime(0);
 
 let startTime = Math.floor(Date.now() / 1000);
-let isOn = true;
 const bell = new Audio("bell.wav");
-const progOnCont = document.getElementById("progress-on-container");
-const progOffCont = document.getElementById("progress-off-container");
-const progOn = document.getElementById("progress-on");
-const progOff = document.getElementById("progress-off");
+const prog = document.getElementById("progress");
+const timer = document.getElementById("timer");
+timer.innerHTML = formatTime(0);
 
 const startTimer = time => {
   startTime = Math.floor(Date.now() / 1000);
-  progOn.style.width = "0";
-  progOff.style.width = "0";
-  if (isOn) {
-    progOnCont.style.height = "8px";
-    progOffCont.style.height = "2px";
-  } else {
-    progOnCont.style.height = "2px";
-    progOffCont.style.height = "8px";
-  }
+  prog.style.width = "0";
   let t = setInterval(function() {
     const diff = Math.floor(Date.now() / 1000) - startTime;
-    document.getElementById("timer").innerHTML = formatTime(diff);
-    if (isOn) progOn.style.width = (diff / time * 100.0).toString() + "%";
-    else progOff.style.width = (diff / time * 100.0).toString() + "%";
+    timer.innerHTML = formatTime(diff);
+    prog.style.width = (diff / time * 100.0).toString() + "%";
     if (diff >= time) {
       clearInterval(t);
       bell.play();
       window.show();
-      if (isOn) {
-        isOn = false;
-        startTimer(Number(document.getElementById("time-off").value) * 60);
-      }
     }
   }, 1000);
 }
 
 document.getElementById("start-btn").addEventListener("click", e => {
-  isOn = true;
-  const timeOn = Number(document.getElementById("time-on").value);
-  const timeOff = Number(document.getElementById("time-off").value);
-  progOnCont.style.width = timeOn / (timeOn + timeOff) * 100.0 + "%";
-  progOffCont.style.width = timeOff / (timeOn + timeOff) * 100.0 + "%";
-  startTimer(timeOn * 60);
+  startTimer(Number(document.getElementById("time").value) * 60);
 });
